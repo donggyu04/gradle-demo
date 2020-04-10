479792f467fceb3343d4638c9392f7d1fd2561a3
donggyu.lee <donggyu.lee@nts-corp.com>
2020-04-10 11:10:16 +0900

    Use dependsOn

--- src/main/java/com/training/gradledemo/GradleDemoApplication.java
+++ src/main/java/com/training/gradledemo/GradleDemoApplication.java
@@ -2,9 +2,10 @@ package com.training.gradledemo;
 
 import com.training.gradledemo.greet.Greeter;
 import org.joda.time.LocalTime;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-//@SpringBootApplication
+@SpringBootApplication
 public class GradleDemoApplication {
     public static void main(String[] args) {
         LocalTime localTime = new LocalTime();
